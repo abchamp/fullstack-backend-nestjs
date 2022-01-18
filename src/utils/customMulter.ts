2478e6136ef42b79521e702c3c59a29783f62463
ChampKatika <inform.katika@gmail.com>
2022-01-18 21:51:49 +0700

    init

---
+++ src/utils/customMulter.ts
@@ -0,0 +1,53 @@
+/**
+ * this file for configuration file options
+ * for this project don't upload file
+ * Reference:
+ * https://stackoverflow.com/questions/49096068/upload-file-using-nestjs-and-multer
+ */
+
+ import { extname } from "path";
+ import { existsSync, mkdirSync } from "fs";
+ import { diskStorage } from 'multer';
+ import { v4 as uuid } from 'uuid';
+ import { HttpException, HttpStatus } from '@nestjs/common';
+ 
+ // Multer configuration
+ export const multerConfig = {
+     dest: "./file_uploads" //process.env.UPLOAD_LOCATION,
+ };
+ 
+ // Multer upload options
+ export const multerOptions = {
+     // Enable file size limits
+     // limits: {
+     //     fileSize: ??
+     // }
+     // Check the mimetypes to allow for upload
+     fileFilter: (req: any, file: any, cb: any) => {
+         if (file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
+             // Allow storage of file
+             cb(null, true);
+         } else {
+             // Reject file
+             cb(new HttpException(`Unsupported file type ${extname(file.originalname)}`, HttpStatus.BAD_REQUEST), false);
+         }
+     },
+     // Storage properties
+     storage: diskStorage({
+         // Destination storage path details
+         destination: (req: any, file: any, cb: any) => {
+             const uploadPath = multerConfig.dest;
+             // Create folder if doesn't exist
+             if (!existsSync(uploadPath)) {
+                 mkdirSync(uploadPath);
+             }
+             cb(null, uploadPath);
+         },
+         // File modification details
+         filename: (req: any, file: any, cb: any) => {
+             // Calling the callback passing the random name generated with the original extension name
+             cb(null, `${uuid()}${extname(file.originalname)}`);
+         },
+     }),
+ }
+ 
---
+++ src/utils/interceptor/transform.interceptor.ts
@@ -0,0 +1,15 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Response<T> {
+    data: T;
+}
+
+@Injectable()
+export class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> {
+    intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
+        console.log("transform interceptor");
+        return next.handle().pipe(map(data => ({ data })));
+    }
+}
